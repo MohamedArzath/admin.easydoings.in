cf649353bfc50553e52434bbb138461fac4cfe6b
arzathaz <arzath.com@gmail.com>
2025-04-13 00:48:52 +0530

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ use App\Http\Controllers\BusinessControllerOg;
 // })->purpose('Display an inspiring quote');
 
 
-Schedule::call(new BusinessControllerOg)->dailyAt('00:48')->timezone('Asia/Kolkata');
+Schedule::call(new BusinessControllerOg)->dailyAt('00:49')->timezone('Asia/Kolkata');
