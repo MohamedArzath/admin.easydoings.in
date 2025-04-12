08bc5dbb39af4ebfb1d350280a78099987c2756a
arzathaz <arzath.com@gmail.com>
2025-04-13 00:36:00 +0530

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\BusinessControllerOg;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
