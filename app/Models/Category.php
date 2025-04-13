777a40b70ebab5d0b02970a84108165a211dc4ff
arzathaz <arzath.com@gmail.com>
2025-04-13 20:31:40 +0530

    admin

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,15 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model {
-    protected $fillable = ['name', 'sector_id', 'category_status','cat_priority'];
+    // protected $fillable = ['name', 'sector_id', 'category_status','cat_priority'];
 
-    public function sector()
-    {
-        return $this->belongsTo(Sector::class);
-    }
-    
-    // protected $connection = 'easydoings';
-    // protected $table = 'categories';
+    // public function sector()
+    // {
+    //     return $this->belongsTo(Sector::class);
+    // }
+     
+    protected $table = 'categories';
 
-    // protected $fillable = ['name', 'slug', 'description']; // Allow mass assignment
+    protected $fillable = ['name', 'slug', 'description']; // Allow mass assignment
 }
